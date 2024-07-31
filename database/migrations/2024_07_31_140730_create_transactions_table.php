d25f76c16ec29863078a52aa190ce0d227647fa9
Hamzani Wahid <hamzaniwahid321@gmail.com>
2024-07-31 22:20:36 +0800

    add role

---
+++ database/migrations/2024_07_31_140730_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->integer('total_harga');
+            $table->integer('diskon');
+            $table->integer('pajak');
+            $table->enum('metode_pembayaran', ['cash', 'transfer', 'qrcode']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
